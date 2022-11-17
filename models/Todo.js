e45f7ee56c934dfddac5b6fc261cf530184805a4
Fahad123k <ahmad.nishu08@gmail.com>
2022-11-17 12:07:07 +0530

    git ig

---
+++ models/Todo.js
@@ -0,0 +1,19 @@
+
+const mongoose = require("mongoose");
+
+const TodoSchema = new mongoose.Schema({
+  todo: {
+    type: String,
+    required: true,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  dueDate: {
+    type: Date,
+    required: true,
+  },
+});
+
+module.exports = new mongoose.model("Todo", TodoSchema);
